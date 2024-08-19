daaa29cde3f2ed14186552971145b5e22079e9ca
David Lee <Dai180293@gmail.com>
2024-08-19 20:46:53 +0100

    added lenis smooth scroll to improve animations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,24 +2,25 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
+import SmoothScrolling from "./SmoothScroll";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
+    title: "Light Saas Landing Page",
+    description: "Template created by Frontend Tribe",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="relative">
-      <body className={clsx(dmSans.className, "antialiased bg-[#EAEEFE] overflow-x-hidden")}>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" className="relative">
+            <body className={clsx(dmSans.className, "antialiased bg-[#EAEEFE] overflow-x-hidden")}>
+                <SmoothScrolling>{children}</SmoothScrolling>
+            </body>
+        </html>
+    );
 }
